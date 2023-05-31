4a013e72de672e5711be807a95090cfbff9ccad7
Douglas <dougbmth@hotmail.com>
2023-05-31 14:54:33 -0300

    feat React App

---
+++ kenzie-kars-front/src/App.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyle } from "./styles/globalStyles"
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Olá</h1>      
+    </div>
+  )
+}
+
+export default App
