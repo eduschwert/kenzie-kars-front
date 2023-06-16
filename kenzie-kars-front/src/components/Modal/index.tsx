ce99e9f59f24e755a023ce0d978bd807f090e9d4
eduschwert <eduardoschwert@yahoo.com.br>
2023-06-16 14:55:09 -0300

    fix modal scroll issue

--- kenzie-kars-front/src/components/Modal/index.tsx
+++ kenzie-kars-front/src/components/Modal/index.tsx
@@ -12,32 +12,58 @@ export const Modal = ({ toggleModal, children, blockClosing }: ModalProps) => {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handleClick = (event: MouseEvent) => {
-      if (!ref.current) {
-        return;
-      }
-
-      if (!event.target) {
-        return;
-      }
-
-      if (
-        !ref.current.contains(event.target as HTMLElement) &&
-        !(
-          event.target instanceof HTMLElement &&
-          event.target.className.includes("Mui")
-        )
-      ) {
+    const handleEscKey = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
         toggleModal();
       }
     };
 
-    window.addEventListener("mousedown", handleClick);
+    const disableScroll = () => {
+      document.body.style.overflow = "hidden";
+    };
+
+    const enableScroll = () => {
+      document.body.style.overflow = "auto";
+    };
+
+    window.addEventListener("keydown", handleEscKey);
+    disableScroll();
 
     return () => {
-      window.removeEventListener("mousedown", handleClick);
+      window.removeEventListener("keydown", handleEscKey);
+      enableScroll();
     };
   }, [toggleModal]);
+
+  useEffect(() => {
+    if (!blockClosing) {
+      const handleClick = (event: MouseEvent) => {
+        if (!ref.current) {
+          return;
+        }
+
+        if (!event.target) {
+          return;
+        }
+
+        if (
+          !ref.current.contains(event.target as HTMLElement) &&
+          !(
+            event.target instanceof HTMLElement &&
+            event.target.className.includes("Mui")
+          )
+        ) {
+          toggleModal();
+        }
+      };
+
+      window.addEventListener("mousedown", handleClick);
+
+      return () => {
+        window.removeEventListener("mousedown", handleClick);
+      };
+    }
+  }, [toggleModal, blockClosing]);
 
   return createPortal(
     <Container>
