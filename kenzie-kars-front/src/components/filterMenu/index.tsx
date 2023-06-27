5e0a31a02b843dfa3f7a8aad6cc122dcfed3e765
JSCFerraz <ferrazjsc@gmail.com>
2023-06-27 15:33:26 -0300

    fix: fecht max price and mileage at api

--- kenzie-kars-front/src/components/filterMenu/index.tsx
+++ kenzie-kars-front/src/components/filterMenu/index.tsx
@@ -20,16 +20,27 @@ import {
   handleSelectedOption,
 } from "./functions";
 import { ProductContext } from "../../contexts/productContext";
+import { api } from "../../services/api";
 
 export const FilterMenu = ({ advertising, setAdvertising }: iAdvertising) => {
-  const { filterConditions, setFilterConditions, filteredProducts } =
-    useContext(ProductContext);
-
-  // States to get values for Nested List
+  const { filterConditions, setFilterConditions } = useContext(ProductContext);
   const [brands, setBrands] = useState([] as Array<string>);
   const [models, setModels] = useState([] as Array<string>);
   const [year, setYear] = useState([] as Array<string>);
   const [fuel, setFuel] = useState([] as Array<string>);
+  const [maxValueOfPrice, setMaxValueOfPrice] = useState<number>(300000);
+  const [maxValueOfMileage, setMaxValueOfMileage] = useState<number>(200000);
+  const [actionOverCarBrand, setActionOverCarBrand] = useState<boolean>(false);
+  const [actionOverCarModel, setActionOverCarModel] = useState<boolean>(false);
+  const [actionOverCarColor, setActionOverCarColor] = useState<boolean>(false);
+  const [actionOverCarYear, setActionOverCarYear] = useState<boolean>(false);
+  const [actionOverCarFuel, setActionOverCarFuel] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState<string>("");
+
+  const minPrice = 0;
+  const maxPrice = maxValueOfPrice;
+  const minMileage = 0;
+  const maxMileage = maxValueOfMileage;
 
   const carColors = [
     "Amarelo",
@@ -45,7 +56,6 @@ export const FilterMenu = ({ advertising, setAdvertising }: iAdvertising) => {
     "Vinho",
   ];
 
-  // Getting Brands from Fipe API
   useEffect(() => {
     const fetchBrands = async () => {
       try {
@@ -59,10 +69,20 @@ export const FilterMenu = ({ advertising, setAdvertising }: iAdvertising) => {
         console.error("Error fetching car database", error);
       }
     };
+
+    const fetchMaxMileagePrice = async () => {
+      try {
+        const response = await api.get("vehicles/max");
+        setMaxValueOfMileage(response.data.maxMileage);
+        setMaxValueOfPrice(response.data.maxPrice);
+      } catch (error) {
+        console.error("Error fetching max mileage and price", error);
+      }
+    };
+    fetchMaxMileagePrice();
     fetchBrands();
   }, []);
 
-  // Getting Models from Fipe API
   useEffect(() => {
     const callbackHandleFilterElements = async () => {
       const filteredCars: Array<string> | void = await handleFilterElements(
@@ -78,7 +98,6 @@ export const FilterMenu = ({ advertising, setAdvertising }: iAdvertising) => {
     callbackHandleFilterElements();
   }, [filterConditions]);
 
-  // Getting Year and Fuel from Fipe API
   useEffect(() => {
     const possibleCarFuel = ["Flex", "Híbrido", "Elétrico"];
 
@@ -97,28 +116,9 @@ export const FilterMenu = ({ advertising, setAdvertising }: iAdvertising) => {
     callbackHandleFilterOptionsYearAndFuel();
   }, [filterConditions]);
 
-  // Getting vehicles from backend
   useEffect(() => {
     handleFilterConditions(filterConditions);
   }, [filterConditions]);
-
-  // Material UI Nested List States to handle Actions
-  const [actionOverCarBrand, setActionOverCarBrand] = useState<boolean>(false);
-  const [actionOverCarModel, setActionOverCarModel] = useState<boolean>(false);
-  const [actionOverCarColor, setActionOverCarColor] = useState<boolean>(false);
-  const [actionOverCarYear, setActionOverCarYear] = useState<boolean>(false);
-  const [actionOverCarFuel, setActionOverCarFuel] = useState<boolean>(false);
-  const [selectedOption, setSelectedOption] = useState<string>("");
-
-  //  const [priceRange, setPriceRange] = useState([0, 100])
-  const maxValueOfPrice = 200000;
-  const maxValueOfMileage = 150000;
-
-  const minPrice = 0;
-  const maxPrice = maxValueOfPrice;
-
-  const minMileage = 0;
-  const maxMileage = maxValueOfMileage;
 
   const [mileageRange, setMileageRange] = useState<number[]>([
     minMileage,
